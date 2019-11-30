2d6765b68640bd3999b8631b043b46df774edcdf
24hawkman <mnb512@vols.utk.edu>
2019-11-30 00:13:13 -0500

    potentially finding the beginning of the listed games on cbs sports

--- MensCollegeBasketball.py
+++ MensCollegeBasketball.py
@@ -4,9 +4,16 @@ from bs4 import BeautifulSoup
 import SMS
 
 
-response_text = requests.get('https://www.espn.com/mens-college-basketball/scoreboard').text
+response_text = requests.get('https://www.cbssports.com/college-basketball/scoreboard/').text
 soup = BeautifulSoup(response_text, 'html.parser')
 html_text = soup.get_text()
+
+start = html_text.find('1\n2\nT\n')
+html_text = html_text[start:len(html_text)]
+
+file = open('html_data.txt', 'w')
+file.write(html_text)
+file.close()
 
 print(html_text)
 
